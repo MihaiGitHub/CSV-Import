af8f59fbbc6979ec090bf2ceae8335a0f61aef13
Mihai Smarandache <mihailiviu@yahoo.com>
2018-04-17 16:43:35 -0700

    Create file upload form and controller method

--- src/Controller/GroupsController.php
+++ src/Controller/GroupsController.php
@@ -11,4 +11,103 @@ class GroupsController extends AppController
        
     }
 
+    public function uploadcsv(){
+        $this->autoRender = false;
+        if ($this->request->is('post')) {
+
+            $filename = $_FILES["persons_file"]["tmp_name"];
+            $handle = fopen($filename, "r");
+            $header = fgetcsv($handle);
+
+            $row = 0;
+            $headers = [];
+            $filepath = $filename;
+
+            if (($handle = fopen($filepath, "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    if (++$row == 1) {
+
+                    $headers = array_flip($data); // Get the column names from the header.
+                    continue;
+
+                    } else {
+
+                    if (array_key_exists('last_name', $headers)) {  // checks every record if its a group or persons record
+
+                            $persons = TableRegistry::get('Persons');
+                            $person = $persons->find()->where(['person_id' => $data[$headers['person_id']]])->first();
+
+                            if (empty($person)) {
+
+                                $person = $persons->newEntity();
+                                
+                                $person->person_id = $data[$headers['person_id']];
+                                $person->first_name = $data[$headers['first_name']];
+                                $person->last_name = $data[$headers['last_name']];
+                                $person->email_address = $data[$headers['email_address']];
+                                $person->group_id = $data[$headers['group_id']];
+                                $person->state = $data[$headers['state']];
+                                $person->created = time();
+                                
+                                if ($persons->save($person)) {
+                                    $id = $person->id;
+                                    
+                                }
+
+                            } else {
+
+                                $person->first_name = $data[$headers['first_name']];
+                                $person->last_name = $data[$headers['last_name']];
+                                $person->email_address = $data[$headers['email_address']];
+                                $person->state = $data[$headers['state']];
+                                $person->modified = time();
+                                
+                                if ($persons->save($person)) {
+                                    $id = $person->id;
+                                    
+                                }
+
+                            }
+
+                    } else {
+
+                            $groups = TableRegistry::get('Groups');
+                            $group = $groups->find()->where(['group_id' => $data[$headers['group_id']]])->first();
+
+                            if (empty($group)) {
+
+                                $group = $groups->newEntity();
+                                
+                                $group->group_id = $data[$headers['group_id']];
+                                $group->group_name = $data[$headers['group_name']];
+                                $group->created = time();
+                                
+                                if ($groups->save($group)) {
+                                    $id = $group->id;
+                                    
+                                }
+
+                            } else {
+
+                                $group->group_name = $data[$headers['group_name']];
+                                $group->modified = time();
+                                
+                                if ($groups->save($group)) {
+                                    $id = $group->id;
+                                    
+                                }
+
+                            }
+
+                    }
+                }
+
+                
+                }
+                fclose($handle);
+            }
+
+        }
+    }
+
 }
--- src/Template/Groups/index.ctp
+++ src/Template/Groups/index.ctp
@@ -1,3 +1,15 @@
+<form id="upload_csv" method="post" enctype="multipart/form-data">
+    <div class="row">
+            <div class="col-md-3">
+                <input type="file" name="persons_file" style="margin-top:15px;" />  
+            </div>  
+            <div class="col-md-3">  
+                <input type="submit" name="upload" id="upload" value="Upload" style="margin-top:10px;" class="btn btn-info" />  
+            </div>  
+    </div>
+</form>
+<br><br>
+
 <table class="table-striped table-hover table-bordered dataTable no-footer jTable" id="groups-table">
     <thead>
         <tr>
